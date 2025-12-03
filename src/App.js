c5c91351d5c70d300a87937e7bfdc46568da90d2
cdfpartridge-web <cdfpartridge@googlemail.com>
2025-12-03 20:07:51 +0000

    Add Google sign-in and refresh ladder UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { useState  } from 'react';
-import Board from './components/board';
-import util from './util';
-import { ActionsContext } from './state/context';
+import RankingDashboard from './components/RankingDashboard';
 
 const App = () => {
-  const [actionType, setActionType] = useState(util.actions.select);
-
-  return (
-    <ActionsContext.Provider value={{ actionType, setActionType }}>
-      <Board/>
-    </ActionsContext.Provider>)
-}
+  return <RankingDashboard />;
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('shows Riftbound ladder headline', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Host lobbies, match quickly, and track Elo/i);
+  expect(heading).toBeInTheDocument();
 });
